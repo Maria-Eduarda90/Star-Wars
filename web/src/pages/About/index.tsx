111db7ecfeb2f4f33b261b011a66ff4d90b9b8ac
Mary Skywalker <snowdennaruto90@gmail.com>
2022-06-19 15:42:17 -0400

    wip page about

--- web/src/pages/About/index.tsx
+++ web/src/pages/About/index.tsx
@@ -2,6 +2,8 @@ import { Cards } from '../../components/Cards';
 import { Header } from '../../components/Header';
 import logoSith from '../../images/svg/logo-sith.svg';
 
+import dartVaderAbout from '../../images/about/dart-vader-about.png';
+
 import styles from './styles.module.scss';
 
 export function About() {
@@ -10,19 +12,22 @@ export function About() {
             <Header />
             <div className={styles.container}>
                 <div className={styles.text}>
+                    <img src={dartVaderAbout} alt="dart vader" />
                     <div>
-                        <img src={logoSith} alt="logo sith" />
-                        <h1><span>C</span>ODIGO <span>S</span>ITH</h1>
-                        <img src={logoSith} alt="logo sith" />
+                        <div>
+                            <img src={logoSith} alt="logo sith" />
+                            <h1><span>C</span>ODIGO <span>S</span>ITH</h1>
+                            <img src={logoSith} alt="logo sith" />
+                        </div>
+                        <p>
+                            A paz é uma mentira, só há paixão.
+                            Através da paixão, eu ganho força.
+                            Através da força, eu ganho poder.
+                            Através do poder, eu ganho vitória.
+                            Através da vitória, as minhas correntes são quebradas.
+                            A Força irá me libertar.
+                        </p>
                     </div>
-                    <p>
-                        A paz é uma mentira, só há paixão.
-                        Através da paixão, eu ganho força.
-                        Através da força, eu ganho poder.
-                        Através do poder, eu ganho vitória.
-                        Através da vitória, as minhas correntes são quebradas.
-                        A Força irá me libertar.
-                    </p>
                 </div>
                 <div className={styles.separator}>Informações relevantes</div>
                 <Cards />
