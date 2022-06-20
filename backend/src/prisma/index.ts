c2da37bf36fe7f2754413e54b21b64feb83455f0
Mary Skywalker <snowdennaruto90@gmail.com>
2022-06-20 10:43:07 -0400

    wip

--- backend/src/prisma/index.ts
+++ backend/src/prisma/index.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-const prismaClient = new PrismaClient();
-
-export default prismaClient;
+export const prisma = new PrismaClient({
+  log: ["query"],
+});
