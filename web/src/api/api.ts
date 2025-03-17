1d943d679857042e8e95d82922fddfdf1594b43f
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2025-03-16 23:40:18 -0300

    pipelines

--- web/src/api/api.ts
+++ web/src/api/api.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-
 export const api = axios.create({
-    baseURL: "https://star-wars-api.xdbtqu.easypanel.host/comments",
+    baseURL: import.meta.env.VITE_API_URL,
 })
